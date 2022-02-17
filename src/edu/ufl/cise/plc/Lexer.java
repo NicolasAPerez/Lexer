c27ef2f331f1286c2067ce1d8a1971dc80089241
NicolasAPerez <nicoperez01@gmail.com>
2022-02-17 00:09:46 -0500

    Finished Lexer

--- src/edu/ufl/cise/plc/Lexer.java
+++ src/edu/ufl/cise/plc/Lexer.java
@@ -4,26 +4,39 @@ import java.util.Map;
 
 public class Lexer implements ILexer{
 
-    private final String toTokens;
-    private int position;
+    private final char[] toTokens;
+    private int column;
     private int line;
-    private State state;
+    private int position;
+
+    private int peekPos;
+    private int peekLine;
+    private int peekCol;
 
     Lexer(String characters){
-        toTokens = characters;
+        //Tilde used as Sentential
+        toTokens = (characters + '\0').toCharArray();
+        column = 0;
+        line = 0;
         position = 0;
     }
 
     private static enum State {
         START,
         HAVE_ZERO,
-        HAVE_DECIMAL,
+        HAVE_DOT,
+        HAVE_EQUAL,
+        HAVE_LARROW,
+        HAVE_RARROW,
+        HAVE_EXCLAIM,
+        HAVE_MINUS,
         IS_INT,
-        STRING_START,
-        STRING_SPECIALCHAR,
         IS_STRING,
+        STRING_ESCAPE,
+        IS_FLOAT,
         IDENTIFIER,
-        COMMENT
+        COMMENT,
+        ENDTOKEN
     }
     private static final Map<String, IToken.Kind> reservedWords = Map.ofEntries(
             Map.entry("int", IToken.Kind.TYPE),
@@ -61,33 +74,460 @@ public class Lexer implements ILexer{
     );
 
     boolean isNumber(char x){
-        switch (x){
-            case '0','1','2','3','4','5','6','7','8','9':
-                return true;
-        }
-        return false;
+        return switch (x) {
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> true;
+            default -> false;
+        };
     }
 
 
     @Override
     public IToken next() throws LexicalException {
-        return null;
+        IToken temp = peek();
+        line = peekLine;
+        column = peekCol;
+        position = peekPos;
+        return temp;
     }
 
     @Override
     public IToken peek() throws LexicalException {
+        //Save old values to reset at the end
+        int oldLine = line;
+        int oldCol = column;
+        int oldPos = position;
+
         IToken.Kind kind = null;
         String val = "";
-        IToken.SourceLocation pos = new IToken.SourceLocation(line,position);
+        IToken.SourceLocation pos = new IToken.SourceLocation(line,column);
         int length = 0;
-        state = State.START;
+        int tokenPos = position;
+        State state = State.START;
 
-        switch (state){
-            case START:
-                switch (toTokens.charAt(position))
+        do {
+            switch (state) {
+                case START -> {
 
+                    switch (toTokens[tokenPos]) {
+                        case '=' -> {
+                            state = State.HAVE_EQUAL;
+                            kind = IToken.Kind.ASSIGN;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '(' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.LPAREN;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case ')' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.RPAREN;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '[' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.LSQUARE;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case ']' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.RSQUARE;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '<' -> {
+                            state = State.HAVE_LARROW;
+                            kind = IToken.Kind.LT;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '>' -> {
+                            state = State.HAVE_RARROW;
+                            kind = IToken.Kind.GT;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '+' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.PLUS;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '-' -> {
+                            state = State.HAVE_MINUS;
+                            kind = IToken.Kind.MINUS;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '*' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.TIMES;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '/' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.DIV;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '%' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.MOD;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '&' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.AND;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '|' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.OR;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '!' -> {
+                            state = State.HAVE_EXCLAIM;
+                            kind = IToken.Kind.BANG;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case ';' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.SEMI;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case ',' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.COMMA;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '^' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.RETURN;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '0' -> {
+                            state = State.HAVE_ZERO;
+                            kind = IToken.Kind.INT_LIT;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '"' -> {
+                            state = State.IS_STRING;
+                            kind = IToken.Kind.INT_LIT;
+                            length++;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '#' -> {
+                            state = State.COMMENT;
+                            length++;
+                        }
+                        case '\0' -> {
+                            state = State.ENDTOKEN;
+                            kind = IToken.Kind.EOF;
+                        }
+                        case '\t', ' ' -> {
+                            state = State.START;
+                            length = 0;
+                            pos = new IToken.SourceLocation(line,column+1);
+                        }
+                        case '\n' ->{
+                            length = 0;
+                            state = State.START;
+                            line++;
+                            column = -1;
+                            pos = new IToken.SourceLocation(line,column+1);
+                        }
+                        case '\r' ->{
+                        }
 
-        }
+                        default -> {
+                            if (Character.isJavaIdentifierStart(toTokens[tokenPos])) {
+                                state = State.IDENTIFIER;
+                                kind = IToken.Kind.IDENT;
+                                length++;
+                                val = val + toTokens[tokenPos];
+
+                            } else if (isNumber(toTokens[tokenPos])) {
+                                state = State.IS_INT;
+                                kind = IToken.Kind.INT_LIT;
+                                length++;
+                                val = val + toTokens[tokenPos];
+                            }
+                            else {
+                                throw new LexicalException("ERROR!");
+                            }
+                        }
+                    }
+                }
+
+                case COMMENT -> {
+                    switch (toTokens[tokenPos]){
+                        case '\r' ->{
+                            isNumber('1');
+                            //Empty for later since \n is next
+                            length++;
+                        }
+                        case '\n' ->{
+                            length = 0;
+                            state = State.START;
+                            line++;
+                            column = -1;
+                            pos = new IToken.SourceLocation(line,column+1);
+                        }
+                        case '\0' ->{
+                            state = State.START;
+                            tokenPos--;
+                            column--;
+                        }
+
+                        default -> {
+                            //Anything else until a newline
+                            length++;
+                        }
+                    }
+                }
+                case HAVE_ZERO -> {
+                    if (toTokens[tokenPos] == '.'){
+                        state = State.HAVE_DOT;
+                        length++;
+                        val = val + toTokens[tokenPos];
+                    }
+                    else {
+                        kind = IToken.Kind.INT_LIT;
+                        val = "0";
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+                case HAVE_DOT -> {
+                    if (isNumber(toTokens[tokenPos])){
+                        state = State.IS_FLOAT;
+                        length++;
+                        val = val + toTokens[tokenPos];
+                    }
+                    else {
+                        throw new LexicalException("Leading float demical");
+
+                    }
+
+                }
+                case HAVE_EXCLAIM -> {
+                    if (toTokens[tokenPos] == '='){
+                        state = State.ENDTOKEN;
+                        val = val + toTokens[tokenPos];
+                        length++;
+                        kind = IToken.Kind.NOT_EQUALS;
+                    }
+                    else {
+                        kind = IToken.Kind.BANG;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+                case HAVE_LARROW -> {
+                    if (toTokens[tokenPos] == '<'){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.LANGLE;
+                    }
+                    else if (toTokens[tokenPos] == '='){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.LE;
+                    }
+                    else if (toTokens[tokenPos] == '-'){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.LARROW;
+                    }
+                    else {
+                        kind = IToken.Kind.LT;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+                case HAVE_RARROW -> {
+                    if (toTokens[tokenPos] == '>'){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.RANGLE;
+                    }
+                    else if (toTokens[tokenPos] == '='){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.GE;
+                    }
+                    else {
+                        kind = IToken.Kind.GT;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+                case HAVE_MINUS -> {
+                    if (toTokens[tokenPos] == '>'){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.RARROW;
+                    }
+                    else {
+                        kind = IToken.Kind.MINUS;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+                case HAVE_EQUAL -> {
+                    if (toTokens[tokenPos] == '='){
+                        val = val + toTokens[tokenPos];
+                        state = State.ENDTOKEN;
+                        length++;
+                        kind = IToken.Kind.EQUALS;
+                    }
+                    else {
+                        kind = IToken.Kind.ASSIGN;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+                case IDENTIFIER -> {
+                    if (Character.isJavaIdentifierPart(toTokens[tokenPos]) && toTokens[tokenPos] != '\0'){
+                        length++;
+                        val = val + toTokens[tokenPos];
+                    }
+                    else {
+                        if (reservedWords.containsKey(val)){
+                            kind = reservedWords.get(val);
+                            state = State.ENDTOKEN;
+                            tokenPos--;
+                            column--;
+                        }
+                        else {
+                            kind = IToken.Kind.IDENT;
+                            state = State.ENDTOKEN;
+                            tokenPos--;
+                            column--;
+                        }
+                    }
+
+                }
+                case IS_INT -> {
+                    if (isNumber(toTokens[tokenPos])){
+                        length++;
+                        val = val + toTokens[tokenPos];
+                    }
+                    else if(toTokens[tokenPos] == '.'){
+                        state = State.HAVE_DOT;
+                        length++;
+                        val = val + toTokens[tokenPos];
+                    }
+                    else {
+                        try{
+                            Integer.parseInt(val);
+                        }
+                        catch (Exception e){
+                            throw new LexicalException("INT TOO LARGE!");
+                        }
+                        kind = IToken.Kind.INT_LIT;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+
+                }
+                case IS_STRING -> {
+                    switch (toTokens[tokenPos]){
+                        case '"' ->{
+                            state = State.ENDTOKEN;
+                            length++;
+                            kind = IToken.Kind.STRING_LIT;
+                            val = val + toTokens[tokenPos];
+                        }
+                        case '\0' ->{
+                            throw new LexicalException("UNBOUNDED STRING!");
+                        }
+                        case '\\' ->{
+                            val = val + toTokens[tokenPos];
+                            length++;
+                            state = State.STRING_ESCAPE;
+                        }
+                        case '\n'->{
+                            column = -1;
+                            line++;
+                            val = val + toTokens[tokenPos];
+                            length++;
+                        }
+                        default -> {
+                            val = val + toTokens[tokenPos];
+                            length++;
+                        }
+                    }
+
+                }
+                case STRING_ESCAPE -> {
+                    switch (toTokens[tokenPos]){
+                        case 'b','t', 'n','f', 'r', '"', '\'','\\'->{
+                            val = val + toTokens[tokenPos];
+                            length++;
+                            state = State.IS_STRING;
+                        }
+                        default -> {
+                            throw new LexicalException("UNMATCHED ESCAPE");
+                        }
+                    }
+                }
+                case IS_FLOAT -> {
+                    if (isNumber(toTokens[tokenPos])){
+                        length++;
+                        val = val + toTokens[tokenPos];
+                    }
+                    else {
+                        try{
+                            Float.parseFloat(val);
+                        }
+                        catch (Exception e){
+                            throw new LexicalException("FLOAT CHECK FAILED!");
+                        }
+                        kind = IToken.Kind.FLOAT_LIT;
+                        state = State.ENDTOKEN;
+                        tokenPos--;
+                        column--;
+                    }
+                }
+
+            }
+            column++;
+            tokenPos++;
+        } while (state != State.ENDTOKEN);
+        peekCol = column;
+        peekPos = tokenPos;
+        peekLine = line;
+
+        column = oldCol;
+        position = oldPos;
+        line = oldLine;
 
 
         return new Token(kind,val,pos,length);
--- src/edu/ufl/cise/plc/Token.java
+++ src/edu/ufl/cise/plc/Token.java
@@ -31,21 +31,81 @@ public class Token implements IToken{
 
     @Override
     public int getIntValue() {
-        return Integer.parseInt(val);
+        if (kind == Kind.INT_LIT) {
+            return Integer.parseInt(val);
+        }
+        else {
+            //ERROR ENCOUNTERED
+            return -1;
+        }
     }
 
     @Override
     public float getFloatValue() {
-        return Float.parseFloat(val);
+        if (kind == Kind.FLOAT_LIT){
+            return Float.parseFloat(val);
+        }
+        else {
+            //ERROR ENCOUNTERED
+            return -1;
+        }
     }
 
     @Override
     public boolean getBooleanValue() {
-        return Boolean.parseBoolean(val);
+        if (kind == Kind.BOOLEAN_LIT) {
+            return Boolean.parseBoolean(val);
+        }
+        else {
+            //ERROR ENCOUNTERED
+            return false;
+        }
     }
 
     @Override
     public String getStringValue() {
-        return val; //TODO: Fix this thing to properly remove exit characters
+        String stringValue = "";
+        for (int i = 0; i < val.length(); i++){
+            if (val.charAt(i) == '\\'){
+                switch (val.charAt(i + 1)){
+                    case 'b' ->{
+                        stringValue = stringValue + '\b';
+                    }
+                    case 't' ->{
+                        stringValue = stringValue + '\t';
+
+                    }
+                    case 'n' ->{
+                        stringValue = stringValue + '\n';
+
+                    }
+                    case 'f' ->{
+                        stringValue = stringValue + '\f';
+
+                    }
+                    case 'r' ->{
+                        stringValue = stringValue + '\r';
+
+                    }
+                    case '"' ->{
+                        stringValue = stringValue + '\"';
+
+                    }
+                    case '\'' ->{
+                        stringValue = stringValue + '\'';
+
+                    }
+                    case '\\' ->{
+                        stringValue = stringValue + '\\';
+                    }
+                }
+                i++;
+            }
+            else if (val.charAt(i) != '"'){
+                stringValue = stringValue + val.charAt(i);
+            }
+
+        }
+        return stringValue;
     }
 }
